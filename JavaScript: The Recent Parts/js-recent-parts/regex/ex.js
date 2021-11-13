41afacd96ea870f5074b536b56cdf2b91447a52c
Andreas Näsman <andreas.nasman@hibox.tv>
2021-11-14 00:40:44 +0200

    Complete exercise

--- JavaScript: The Recent Parts/js-recent-parts/regex/ex.js
+++ JavaScript: The Recent Parts/js-recent-parts/regex/ex.js
@@ -1,3 +1,12 @@
+function* powers(poem) {
+  const regex = /(?<=power of )(?<noun>(?:a )?\w+).+?(?<=can )(?<verb>\w+)/gs;
+  let match;
+  while ((match = regex.exec(poem))) {
+    const { groups: { noun, verb } = {} } = match;
+    yield `${noun}: ${verb}`;
+  }
+}
+
 // The Power of a Smile
 // by Tupac Shakur
 var poem = `
@@ -11,7 +20,7 @@ but the power of a smile
 especially yours can heal a frozen heart`;
 
 for (let power of powers(poem)) {
-	console.log(power);
+  console.log(power);
 }
 // a gun: kill
 // fire: burn
@@ -19,7 +28,6 @@ for (let power of powers(poem)) {
 // a mind: learn
 // anger: rage
 // smile: heal
-
 
 // Hints:
 //
